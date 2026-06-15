870fb502ec31a5b13992cfc53bab4adb3b39aeff
Yassine El khader <yael-kha@c2r9p7.1337.ma>
2026-06-15 19:05:03 +0100

    dot.h

--- codexion.h
+++ codexion.h
@@ -1,7 +1,20 @@
 #ifndef CODEXION_H
 #define CODEXION_H
 
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct  s_input
+{
+    int number_of_coders;
+    int time_to_burnout;
+    int time_to_compile;
+    int time_to_debug;
+    int time_to_refactor;
+    int number_of_compiles_required;
+    int dongle_cooldown;
+    char *scheduler;
+}   t_input;
 
 #endif
