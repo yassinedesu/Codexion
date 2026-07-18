fad865d145f14ed6f5ca4311eca95845c9ff5252
Yassine El khader <yael-kha@c1r1p14.1337.ma>
2026-07-18 13:08:09 +0100

    codexion.h/updated

--- src/codexion.h
+++ src/codexion.h
@@ -14,63 +14,67 @@
 # define CODEXION_H
 
 # include <pthread.h>
+# include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <stdbool.h>
 # include <string.h>
-# include <unistd.h>
 # include <sys/time.h>
+# include <unistd.h>
 
 # define MAX_INT 2147483647
 
 typedef struct s_input
 {
-	int			number_of_coders;
-	int			time_to_burnout;
-	int			time_to_compile;
-	int			time_to_debug;
-	int			time_to_refactor;
-	int			number_of_compiles_required;
-	int			dongle_cooldown;
-	char		*scheduler;
-}				t_input;
+	int					number_of_coders;
+	int					time_to_burnout;
+	int					time_to_compile;
+	int					time_to_debug;
+	int					time_to_refactor;
+	int					number_of_compiles_required;
+	int					dongle_cooldown;
+	char				*scheduler;
+}						t_input;
+
+typedef struct s_sim	t_sim;
 
 typedef struct s_coder
 {
-	pthread_t	thread;
-	int			coder_id;
-	int			number_of_compiles;
-	long		last_compile_start;
-	t_sim		*data;
-}				t_coder;
+	pthread_t			thread;
+	int					coder_id;
+	int					number_of_compiles;
+	long				last_compile_start;
+	t_sim				*data;
+}						t_coder;
 
-typedef struct	s_dongle
+typedef struct s_dongle
 {
-	pthread_mutex_t	mutex;
-	int	id;
-	bool	is_taken;
-	long	last_time_used;
-	pthread_cond_t	cond; // what's a pthread_cond_t cond?
-}				t_dongle;
+	pthread_mutex_t		mutex;
+	int					id;
+	bool				is_taken;
+	long				last_time_used;
+	pthread_cond_t		cond; // what's a pthread_cond_t cond?
+}						t_dongle;
 
-typedef struct	s_sim
+typedef struct s_sim
 {
-	int	num_of_coders; 
-	t_input	*params;
-	t_coder	coders;
-	t_dongle	*dongles;
-	struct	timeval	t_zero; 
-	pthread_mutex_t	log_mutex;
-	int	stop_flag;
-	pthread_mutex_t	stop_mutex; // why this one & the stop_flag are related to each other, the stop_flag is the signal, does that mean that stop_mutex is the one cleaning up?
-	pthread_t	monitor;
-
-}				t_sim;
+	int					num_of_coders;
+	t_input				*params;
+	t_coder				coders;
+	t_dongle			*dongles;
+	struct timeval		t_zero;
+	pthread_mutex_t		log_mutex;
+	int					stop_flag;
+	pthread_mutex_t		stop_mutex;
+	// why this one & the stop_flag are related to each other
+	// ,the stop_flag is the signal,
+	// does that mean that stop_mutex is the one cleaning up ? 
+	pthread_t			monitor;
+}						t_sim;
 
 // parser
-t_input			*parsed_args(int argc, char **argv);
+t_input					*parsed_args(int argc, char **argv);
 
 // monitor
-void			*mon(void *arg);
+void					*mon(void *arg);
 
 #endif
