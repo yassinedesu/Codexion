47faf6a22536b91a2d5ac765ed22bc88b7b82a44
tor2torporfavor <tor2torporfavor@kali>
2026-07-18 17:29:03 +0100

    init.c/init_coders_edit

--- src/init.c
+++ src/init.c
@@ -12,22 +12,28 @@
 
 #include "codexion.h"
 
-t_coder *init_coders(t_input *params)
+t_coder	*init_coders(t_sim *sims)
 {
-    int     i;
-    int     stat;
-    t_coder *coders;
+	int		i;
+	t_coder	*coders;
 
-    coders = malloc(sizeof(t_coder *) * params->number_of_coders);
-    if (!coders)
-        return (NULL);
-    i = 0;
-    while (i < params->number_of_coders)
-    {
-        coders[i].coder_id = i + 1;
-        coders[i].number_of_compiles = 0;
-        coders[i].last_compile_start = 0;
-        i++;
-    }
-    return (coders);
+	coders = malloc(sizeof(t_coder *) * sims->params->number_of_coders);
+	if (!coders)
+		return (NULL);
+	i = 0;
+	while (i < sims->params->number_of_coders)
+	{
+		memset(&coders[i], NULL, sims->params->number_of_coders);
+		i++;
+	}
+	i = 0;
+	while (i < sims->params->number_of_coders)
+	{
+		coders[i].sim = sims;
+		coders[i].coder_id = i + 1;
+		coders[i].number_of_compiles = 0;
+		coders[i].last_compile_start = 0;
+		i++;
+	}
+	return (coders);
 }
