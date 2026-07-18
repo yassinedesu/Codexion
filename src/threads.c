6959134b3efb7510d2c1186e9f4560caf8649292
tor2torporfavor <tor2torporfavor@kali>
2026-07-18 23:20:02 +0100

    src/threads.c

--- src/threads.c
+++ src/threads.c
@@ -6,25 +6,26 @@
 /*   By: yael-kha <yael-kha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/18 23:12:00 by yael-kha          #+#    #+#             */
-/*   Updated: 2026/07/18 23:12:11 by yael-kha         ###   ########.fr       */
+/*   Updated: 2026/07/18 23:19:49 by yael-kha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-t_sim   *coder_create(t_sim *sims)
+t_sim	*coder_create(t_sim *sims)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < sims->params->number_of_coders)
-    {
-        if (pthread_create(&sims->coders[i].thread, NULL, coder_routine, sims->coders) != 0)
-        {
-            free_all(sims);
-            return (NULL);
-        }
-        i++;
-    }
-    return (sims);
+	i = 0;
+	while (i < sims->params->number_of_coders)
+	{
+		if (pthread_create(&sims->coders[i].thread, NULL, coder_routine,
+				sims->coders) != 0)
+		{
+			free_all(sims);
+			return (NULL);
+		}
+		i++;
+	}
+	return (sims);
 }
