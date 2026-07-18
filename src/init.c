04a98b3a19e5978baa029e518a4c242d3578c006
tor2torporfavor <tor2torporfavor@kali>
2026-07-18 17:17:48 +0100

    init.c/init_coders

--- src/init.c
+++ src/init.c
@@ -12,3 +12,22 @@
 
 #include "codexion.h"
 
+t_coder *init_coders(t_input *params)
+{
+    int     i;
+    int     stat;
+    t_coder *coders;
+
+    coders = malloc(sizeof(t_coder *) * params->number_of_coders);
+    if (!coders)
+        return (NULL);
+    i = 0;
+    while (i < params->number_of_coders)
+    {
+        coders[i].coder_id = i + 1;
+        coders[i].number_of_compiles = 0;
+        coders[i].last_compile_start = 0;
+        i++;
+    }
+    return (coders);
+}
