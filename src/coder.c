3ef696a9f101e60c626ad590091b25be2563c81f
tor2torporfavor <tor2torporfavor@kali>
2026-07-20 15:43:22 +0100

    src/coder.c <--> asymmetric_acq

--- src/coder.c
+++ src/coder.c
@@ -15,7 +15,13 @@
 void	*coder_routine(void *arg)
 {
 	t_coder	*coder;
-
+	int	right_dongle;
+	
+	right_dongle = coder->coder_id % coder->sim->num_of_coders;
+	if (coder->coder_id % 2 == 0)
+		take_dongle(coder, coder->coder_id - 1);
+	else
+		take_dongle(coder, right_dongle);
 	coder = (t_coder *)arg;
 	print_status(coder, "has taken a dongle");
 	return (NULL);
