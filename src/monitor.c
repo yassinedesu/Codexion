a53a798edc9af1c403ead09139f0ae1193038dd1
tor2torporfavor <tor2torporfavor@kali>
2026-07-21 02:15:51 +0100

    monitor_routine

--- src/monitor.c
+++ src/monitor.c
@@ -15,8 +15,32 @@
 void	*monitor_routine(void *arg)
 {
 	t_sim	*sim;
+	int		i;
+	int		last_comp;
 
 	sim = (t_sim *)arg;
-	printf("%d of %s", sim->params->number_of_coders, sim->params->scheduler);
+	last_comp = sim->params->time_to_burnout;
+	while (1)
+	{
+		i = 0;
+		while (i < sim->params->number_of_coders)
+		{
+			if (timestamp_calc(sim->t_zero) - last_comp 
+				>= sim->params->time_to_burnout)
+			{
+				pthread_mutex_lock(&sim->stop_mutex);
+				if (sim->stop_flag == 1)
+				{
+					pthread_mutex_unlock(&sim->stop_mutex);
+					break;
+				}
+				sim->stop_flag = 1;
+				print_status(&sim->coders[i], "burned out");
+				// idk what helper function you're talking about
+			}
+			i++;
+		}
+		pthread_mutex_unlock(&sim->stop_mutex);
+	}
 	return (NULL);
 }
