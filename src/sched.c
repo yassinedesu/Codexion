36cebe297792a7f62684b0851032d821999bd6ba
tor2torporfavor <tor2torporfavor@kali>
2026-07-22 17:17:36 +0100

    sched.c

--- src/sched.c
+++ src/sched.c
@@ -9,3 +9,17 @@
 /*   Updated: 2026/07/17 15:03:06 by yael-kha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "codexion.h"
+
+long    get_priority(t_coder *coder)
+{
+    long    last_compile;
+
+    if (strcmp(coder->sim->params->scheduler, "fifo"))
+        return (timestamp_calc(coder->sim->t_zero));
+    pthread_mutex_lock(&coder->sim->stop_mutex);
+    last_compile = coder->last_compile_start;
+    pthread_mutex_unlock(&coder->sim->stop_mutex);
+    return (last_compile + coder->sim->params->time_to_burnout);
+}
