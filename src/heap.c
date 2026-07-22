ed6bf634293afc01ab8cf933ae92205e5fb6605f
Yassine El khader <yael-kha@c1r9p3.1337.ma>
2026-07-22 04:54:48 +0100

    init_heap(int cap)

--- src/heap.c
+++ src/heap.c
@@ -14,9 +14,11 @@
 
 // creating init_heap, heap_push & heap_pop
 
-// Create an init_heap function that accepts an integer for capacity. It should allocate
-// the t_heap structure, allocate the internal t_node array based on that capacity,
-// set the size to 0, and return the allocated heap pointer.
+/*Create a heap_push function that takes the heap pointer, the coder_id, and a long for priority.
+  Insert the new node's data at the index of the current size, then increment the size. To sort it,
+  write a while loop that compares this new node against its parent 
+  (found at index (current_index - 1) / 2). If the new node has a smaller priority, swap them. 
+  Keep bubbling up until it hits the root or is no longer smaller than its parent.*/
 
 t_heap	*init_heap(int cap)
 {
@@ -28,8 +30,18 @@ t_heap	*init_heap(int cap)
 		return (NULL);
     node = malloc(sizeof(t_node) * cap);
     if (!node)
+    {
+        free(heaps);
         return (NULL);
+    }
     heap->array = node;
     heaps->size = 0;
 	return (heaps);
+}
+t_heap  *heap_push(t_heap *heaps, int c_id, long prior)
+{
+    while (heaps->size < heaps->capacity)
+    {
+
+    }
 }
