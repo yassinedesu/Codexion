98f752dee50dfeeda5da70f69566591d3618fe4d
Yassine El khader <yael-kha@c1r9p3.1337.ma>
2026-07-22 04:02:24 +0100

    Day-3 done

--- src/log.c
+++ src/log.c
@@ -17,6 +17,14 @@ void	print_status(t_coder *coder, char *status)
 	long	millisec;
 
 	pthread_mutex_lock(&coder->sim->log_mutex);
+	pthread_mutex_lock(&coder->sim->stop_mutex);
+	if (coder->sim->stop_flag == 1 && strcmp(status, "burned out") != 0)
+	{
+		pthreat_mutex_unlock(&coder->sim-stop_mutex);
+		pthreat_mutex_unlock(&coder->sim-log_mutex);
+		return ;
+	}
+	pthread_mutex_unlock(&coder->sim->stop_mutex);
 	millisec = timestamp_calc(coder->sim->t_zero);
 	printf("%ld %d %s\n", millisec, coder->coder_id, status);
 	pthread_mutex_unlock(&coder->sim->log_mutex);
